c4748b131e2e4b669b8c262d0cf47b16fd57a903
Sam <stschoberg@gmail.com>
2019-07-01 21:23:51 -0400

    format twitter

--- react-client/src/components/HomePage.jsx
+++ react-client/src/components/HomePage.jsx
@@ -3,7 +3,10 @@ import Container from '@material-ui/core/Container';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
 class HomePage extends Component {
 
@@ -12,17 +15,24 @@ class HomePage extends Component {
   }
     render() {
       return (
-        <div>
-        <AppBar position="static" style={{textAlign: 'center', height:'100px', backgroundColor:"#005b96"}}>
+        <div style={{height:'100%'}}>
+        <AppBar position="static" style={{textAlign: 'center', height:'100%', backgroundColor:"#005b96"}}>
         <Toolbar>
           <Typography variant="h6" style={{flexGrow: 1}}>
             samschoberg
           </Typography>
         </Toolbar>
         </AppBar>
-        <Container style={{width:'100%', height:'1000px', backgroundColor:'#e8f2f9'}}>
-        <a class="twitter-timeline" href="https://twitter.com/stschoberg?ref_src=twsrc%5Etfw">Tweets by stschoberg</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </Container>
+
+        <Card style={{width:'30%', height:'600px'}}>
+      <CardContent style={{width:'100%'}}>
+      <a class="twitter-timeline" data-width="100%" data-height="500px" href="https://twitter.com/stschoberg?ref_src=twsrc%5Etfw">Tweets by stschoberg</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>      </CardContent>
+      <CardActions>
+        <Button size="large">Stalk my Twitter</Button>
+      </CardActions>
+    </Card>
+
+
         </div>
     )
       }
