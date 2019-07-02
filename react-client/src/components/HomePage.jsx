0a97159e358b42d0960128e7db2b85a65fc5b138
Sam <stschoberg@gmail.com>
2019-07-01 22:26:25 -0400

    shitty carousel working

--- react-client/src/components/HomePage.jsx
+++ react-client/src/components/HomePage.jsx
@@ -7,6 +7,9 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
 
 class HomePage extends Component {
 
@@ -14,6 +17,16 @@ class HomePage extends Component {
     super(props);
   }
     render() {
+      var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+
+
+
       return (
         <div style={{height:'100%'}}>
         <AppBar position="static" style={{textAlign: 'center', height:'100%', backgroundColor:"#005b96"}}>
@@ -26,13 +39,29 @@ class HomePage extends Component {
         </Toolbar>
         </AppBar>
 
-        <Card style={{width:'30%', height:'600px'}}>
+
+
+        <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={125}
+        totalSlides={3}
+      >
+        <Slider>
+          <Slide index={0}>        <Card style={{width:'30%', height:'600px', margin:'20px'}}>
       <CardContent style={{width:'100%'}}>
       <a class="twitter-timeline" data-width="100%" data-height="500px" href="https://twitter.com/stschoberg?ref_src=twsrc%5Etfw">Tweets by stschoberg</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>      </CardContent>
       <CardActions>
         <Button size="large">Stalk my Twitter</Button>
       </CardActions>
-    </Card>
+    </Card></Slide>
+          <Slide index={1}>I am the second Slide.</Slide>
+          <Slide index={2}>I am the third Slide.</Slide>
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+      </CarouselProvider>
+
+
 
 
         </div>
