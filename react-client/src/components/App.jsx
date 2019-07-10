8d16c5c1469feb41e4f025fd37d853bd621bda60
Sam <stschoberg@gmail.com>
2019-07-09 22:38:09 -0400

    google analytics

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -4,8 +4,16 @@ import { browserHistory } from 'react-router';
 import HomePage from './HomePage.jsx';
 import NavBar from './NavBar.jsx';
 import Footer from './Footer.jsx';
+import ReactGA from 'react-ga';
+import auth from './auth.ts'; // Sample authentication provider
+const trackingId = "UA-143618936-1"; 
+
 
 class App extends Component {
+
+  // Replace with your Google Analytics tracking ID
+  ReactGA.initialize(trackingId);
+  ReactGA.set({userId: auth.currentUserId()});
   render() {
     return (
       <Router>
