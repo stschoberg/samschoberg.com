0cda13cc4d8ad1492cb8aded09ee5e346bfdb231
Sam <stschoberg@gmail.com>
2019-07-16 20:55:59 -0400

    cool things tab

--- react-client/src/components/HomePage.jsx
+++ react-client/src/components/HomePage.jsx
@@ -37,7 +37,7 @@ class HomePage extends Component {
 
 
       return (
-        <div>
+        <div style={{width:'100%', height:'100%'}}>
         <AppBar position="static" style={{textAlign: 'center', height:'100%', backgroundColor:"#005b96"}}>
         <Toolbar>
         <Typography variant="h6">Terp.</Typography>
@@ -48,20 +48,51 @@ class HomePage extends Component {
         </Toolbar>
         </AppBar>
 
-        <div style={{paddingLeft: '12%'}}>
+        <div style={{paddingLeft:'13%', alignContent:'center'}}>
         <Grid container spacing={3}>
-        <Grid item xs={5} style={{paddingTop:'2%', height:'100px'}}>
-          <Paper >
-          <a class="twitter-timeline" data-height='600'  href="https://twitter.com/stschoberg?ref_src=twsrc%5Etfw">Tweets by stschoberg</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-          </Paper>
-        </Grid>
+
         <Grid item xs={5} style={{paddingTop:'2%', textAlign:'center'}}>
           <Paper > 
             <a href={path} target='_blank'><img style={{'max-width':'100%', 'max-height':'100%'}}src={resumeImage}/></a>
             <Typography variant='h6'>Click for PDF</Typography>
           </Paper>
         </Grid>
+        <Grid item  xs={5} style={{paddingTop:'2%', height:'100px'}}>
+          <Paper >
+          <a class="twitter-timeline" data-height='600'  href="https://twitter.com/stschoberg?ref_src=twsrc%5Etfw">Tweets by stschoberg</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+          </Paper>
+        </Grid>
+
       </Grid>
+
+      <Grid item  xs={5} style={{paddingTop:'2%', height:'100px'}}>
+          <Paper >
+          <Typography variant='h2'>Cool Things Right Now: </Typography>
+          <ul style={{paddingTop:'10px'}}>
+            <li>Social Network Analysis</li>
+              <ul>
+                <li>Started using graph databases at work and wanted to learn more about applications to social media. </li>
+                <li><a href="https://www.amazon.com/Social-Network-Analysis-Startups-connections/dp/1449306462">Analyzing social networks for Startups.</a></li>
+                <li><a href="https://github.com/stschoberg/SocialNetworkAnalysis">Some code.</a></li>
+
+                
+              </ul>
+            <li>Summer Books</li>
+              <ul>
+                <li>All The Light we Cannot See by Anthony Doerr</li>
+                <li>Shoe Dog by Phil Knight</li>
+
+              </ul>
+            <li>This Site</li>
+            <ul>
+                <li>A back end to submit good quotes from summer books?</li>
+                <li>Make responsive.</li>
+                <li>Showcase some of social network analysis.</li>
+
+              </ul>
+          </ul>
+          </Paper>
+        </Grid>
 
       </div>
 
