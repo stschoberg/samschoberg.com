89d2a91a0feb140965b82ab6fe689a2ce2e2921a
Sam <stschoberg@gmail.com>
2020-03-31 21:36:12 -0400

    updated build and deploy script

---
+++ build/precache-manifest.a1d5b57ddab5bf22d5c85505a217b0c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ff5fcd9795be0ca893239ccda1f50d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1541133f14d43c1b3046",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d1976238e77f3ab0382f",
+    "url": "/static/css/main.24069f3f.chunk.css"
+  },
+  {
+    "revision": "1541133f14d43c1b3046",
+    "url": "/static/js/2.8385f105.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8385f105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1976238e77f3ab0382f",
+    "url": "/static/js/main.d88cc78c.chunk.js"
+  },
+  {
+    "revision": "fbde89f77ac1e47efdb6",
+    "url": "/static/js/runtime-main.e5a347a1.js"
+  },
+  {
+    "revision": "9ffa5c934f04ad6bb6f73de0a4053daf",
+    "url": "/static/media/profilePic.9ffa5c93.jpg"
+  }
+]);
